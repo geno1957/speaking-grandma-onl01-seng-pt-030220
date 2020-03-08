952d15d59674d10e84f33461b60ec8bc162c7879
organized-trainer-0561 <jared.epps322@gmail.com>
2020-03-08 20:19:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-def speak_to_grandma( sentence)
+def speak_to_grandma(sentence)
   if (sentence == ("I LOVE YOU GRANDMA!"))
     return "I LOVE YOU TOO PUMPKIN!"
     elsif (sentence == sentence.upcase)
